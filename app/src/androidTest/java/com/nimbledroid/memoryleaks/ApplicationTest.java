de7d73508012ee7150923dccb0b543d3c15573a5
Yongqiang Tan <yongqiang@nimbledroid.com>
2016-05-06 18:23:45 -0400

    espresso test

--- app/src/androidTest/java/com/nimbledroid/memoryleaks/ApplicationTest.java
+++ app/src/androidTest/java/com/nimbledroid/memoryleaks/ApplicationTest.java
@@ -1,13 +1,55 @@
 package com.nimbledroid.memoryleaks;
 
-import android.app.Application;
-import android.test.ApplicationTestCase;
 
-/**
- * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.app.Activity;
+import android.support.test.espresso.action.ViewActions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import java.io.IOException;
+
+import nimbledroid.trace.Profile;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import nimbledroid.trace.Profile;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ApplicationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void staticActivityTest() {
+        start();
+        onView(withId(R.id.sa_button)).perform(click());
+
+        // This view is in a different Activity, no need to tell Espresso.
+        onView(withId(R.id.leak_text)).check(matches(withText("You've triggered a memory leak")));
+        stop();
+    }
+
+    private void start() {
+        Profile.startProfile();
+        android.util.Log.i("NDProfiler", "test started at "+System.currentTimeMillis());
+    }
+
+    private void stop() {
+        android.util.Log.i("NDProfiler", "test stopped at "+System.currentTimeMillis());
+        Profile.startProfile();
     }
 }
---
+++ app/src/androidTest/java/nimbledroid/trace/Profile.java
@@ -0,0 +1,10 @@
+package nimbledroid.trace;
+
+/**
+ * Created by yongqiang on 5/6/16.
+ */
+public class Profile {
+    public static native void startProfile();
+
+    public static native void stopProfile();
+}
